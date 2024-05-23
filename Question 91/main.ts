d58df6d6a26e41556b25ceb2971077af616ace68
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-23 20:32:20 +0500

    adding 31 day of challenge

---
+++ Question 91/main.ts
@@ -0,0 +1,7 @@
+// *Question 91:* Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+// *Explain & TIP:* Adding elements to an array can be easily done using the .push() method. It allows you to add new items to the end of an array.
+
+let my_fruit_basket :string[]=["Apple","Banana","Orange"]
+my_fruit_basket .push("Watermelon")
+console.log(my_fruit_basket)
